41caa25f37e064a72c8b0dcc7eb4265802ba1367
Dodmanat <Dodmanat@yandex.ru>
2022-10-19 12:27:06 +0300

    ПÐНа ревью

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# api_final
-api final
+Как запустить проект:
+Клонировать репозиторий и перейти в него в командной строке:
+
+
+cd api_final_yatube
+Cоздать и активировать виртуальное окружение:
+
+python -m venv env
+source venv/bin/activate
+python -m pip install --upgrade pip
+Установить зависимости из файла requirements.txt:
+
+pip install -r requirements.txt
+Выполнить миграции:
+
+python manage.py migrate
+Запустить проект:
+
+python manage.py runserver
--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,8 +1,8 @@
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
+from rest_framework.validators import UniqueTogetherValidator
 
-
-from posts.models import Comment, Post
+from posts.models import Comment, Follow, Group, Post, User
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -21,3 +21,36 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         fields = '__all__'
         model = Comment
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = ('id', 'title', 'slug', 'description')
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        slug_field='username',
+        queryset=User.objects.all(),
+        default=serializers.CurrentUserDefault())
+    following = serializers.SlugRelatedField(
+        slug_field='username',
+        queryset=User.objects.all())
+
+    class Meta:
+        model = Follow
+        fields = '__all__'
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=('user', 'following'),
+                message='Уже существует подписка на данного автора.'
+            )
+        ]
+
+    def validate(self, data):
+        if data['user'] == data['following']:
+            raise serializers.ValidationError(
+                'Невозможно подписаться на самого себя!')
+        return data
